d21a630c618255fdc5286e96f3ba8a41b69f9779
Aaron_Y <amyerke@uncc.edu>
2020-02-26 21:20:04 -0500

    pipeline added but not tested

--- philr_pipelines/dada2_phangorn_phyloseq_philr.R
+++ philr_pipelines/dada2_phangorn_phyloseq_philr.R
@@ -2,6 +2,7 @@
 # This is a pipeline that was created to explore the philr transform.
 # Many resources were used to create this pipeline:
 #   Processing the sequences through DADA2, making trees with phangorn, and putting them into phyloseq objects:
+#     This is by far the biggest source:
 #     https://github.com/spholmes/F1000_workflow/blob/master/MicrobiomeWorkflow/MicrobiomeWorkflowII.Rmd
 #     or (in article format)
 #     https://f1000research.com/articles/5-1492/v2
@@ -16,15 +17,19 @@
 
 rm(list = ls()) #clear workspace
 
-# Install philr
-# try http:// if https:// URLs are not supported
-if (!requireNamespace("BiocManager", quietly=TRUE))
-    install.packages("BiocManager")
-# BiocManager::install("BiocUpgrade") ## you may need this
-BiocManager::install("philr")
-library("philr")
-library(dada2); packageVersion("dada2")
-
+library("knitr")
+library("BiocStyle")
+.cran_packages <- c("ggplot2", "gridExtra")
+.bioc_packages <- c("dada2", "phyloseq", "DECIPHER", "phangorn")
+.inst <- .cran_packages %in% installed.packages()
+if(any(!.inst)) {
+  install.packages(.cran_packages[!.inst])
+}
+.inst <- .bioc_packages %in% installed.packages()
+if(any(!.inst)) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite(.bioc_packages[!.inst], ask = F)
+}
 
 home_dir = file.path('~','git','Western_gut')
 output_dir = file.path('~','git','Western_gut', 'output')
@@ -77,3 +82,143 @@ seqtabNoC <- removeBimeraDenovo(seqtabAll)
 fastaRef <- file.path(home_dir, 'philr_pipelines', "taxonomy", "./rdp_train_set_16.fa.gz")
 taxTab <- assignTaxonomy(seqtabNoC, refFasta = fastaRef, multithread=TRUE)
 unname(head(taxTab))
+
+seqs <- getSequences(seqtabNoC)
+names(seqs) <- seqs # This propagates to the tip labels of the tree
+alignment <- AlignSeqs(DNAStringSet(seqs), anchor=NA,verbose=FALSE)
+
+phangAlign <- phyDat(as(alignment, "matrix"), type="DNA")
+dm <- dist.ml(phangAlign)
+treeNJ <- NJ(dm) # Note, tip order != sequence order
+fit = pml(treeNJ, data=phangAlign)
+fitGTR <- update(fit, k=4, inv=0.2)
+fitGTR <- optim.pml(fitGTR, model="GTR", optInv=TRUE, optGamma=TRUE,
+                    rearrangement = "stochastic", control = pml.control(trace = 0))
+detach("package:phangorn", unload=TRUE)
+
+samdf <- read.csv("https://raw.githubusercontent.com/spholmes/F1000_workflow/master/data/MIMARKS_Data_combined.csv",header=TRUE)
+samdf$SampleID <- paste0(gsub("00", "", samdf$host_subject_id), "D", samdf$age-21)
+samdf <- samdf[!duplicated(samdf$SampleID),] # Remove dupicate entries for reverse reads
+rownames(seqtabAll) <- gsub("124", "125", rownames(seqtabAll)) # Fix discrepancy
+all(rownames(seqtabAll) %in% samdf$SampleID) # TRUE
+rownames(samdf) <- samdf$SampleID
+keep.cols <- c("collection_date", "biome", "target_gene", "target_subfragment",
+               "host_common_name", "host_subject_id", "age", "sex", "body_product", "tot_mass",
+               "diet", "family_relationship", "genotype", "SampleID") 
+samdf <- samdf[rownames(seqtabAll), keep.cols]
+
+ps <- phyloseq(otu_table(seqtabNoC, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxTab),phy_tree(fitGTR$tree))
+ps <- prune_samples(sample_names(ps) != "Mock", ps) # Remove mock sample
+ps
+
+# Install philr
+# try http:// if https:// URLs are not supported
+if (!requireNamespace("BiocManager", quietly=TRUE))
+  install.packages("BiocManager")
+# BiocManager::install("BiocUpgrade") ## you may need this
+BiocManager::install("philr")
+library("philr")
+
+ps <-  filter_taxa(ps, function(x) sum(x > 3) > (0.2*length(x)), TRUE)
+ps <-  filter_taxa(ps, function(x) sd(x)/mean(x) > 3.0, TRUE)
+ps <- transform_sample_counts(ps, function(x) x+1)
+
+ps
+
+G_tree <- phy_tree(ps)
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+is.rooted(phy_tree(ps)) # Is the tree Rooted?
+is.binary.tree(phy_tree(ps)) # All multichotomies resolved?
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+phy_tree(ps) <- makeNodeLabel(phy_tree(ps), method="number", prefix='n')
+
+## --------------------------------------------------------------------------
+name.balance(phy_tree(ps), tax_table(ps), 'n1')
+
+## --------------------------------------------------------------------------
+otu.table <- t(otu_table(ps))
+tree <- phy_tree(ps)
+metadata <- sample_data(ps)
+tax <- tax_table(ps)
+
+otu.table[1:2,1:2] # OTU Table
+tree # Phylogenetic Tree
+head(metadata,2) # Metadata
+head(tax,2) # taxonomy table
+
+## --------------------------------------------------------------------------
+ps.philr <- philr(otu.table, tree, 
+                  part.weights='enorm.x.gm.counts', 
+                  ilr.weights='blw.sqrt')
+ps.philr[1:5,1:5]
+
+## --------------------------------------------------------------------------
+ps.dist <- dist(ps.philr, method="euclidean")
+ps.pcoa <- ordinate(ps, 'PCoA', distance=ps.dist)
+plot_ordination(ps, ps.pcoa, color='SampleType') + geom_point(size=4)
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+sample_data(ps)$human <- factor(get_variable(ps, "SampleType") %in% c("Feces", "Mock", "Skin", "Tongue"))
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+library(glmnet); packageVersion('glmnet')
+glmmod <- glmnet(ps.philr, sample_data(ps)$human, alpha=1, family="binomial")
+
+## --------------------------------------------------------------------------
+top.coords <- as.matrix(coefficients(glmmod, s=0.2526))
+top.coords <- rownames(top.coords)[which(top.coords != 0)]
+(top.coords <- top.coords[2:length(top.coords)]) # remove the intercept as a coordinate
+
+## --------------------------------------------------------------------------
+tc.names <- sapply(top.coords, function(x) name.balance(tree, tax, x))
+tc.names
+
+## --------------------------------------------------------------------------
+votes <- name.balance(tree, tax, 'n730', return.votes = c('up', 'down'))
+
+votes[[c('up.votes', 'Family')]] # Numerator at Family Level
+votes[[c('down.votes', 'Family')]] # Denominator at Family Level
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+library(ggtree); packageVersion("ggtree")
+library(dplyr); packageVersion('dplyr')
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+tc.nn <- name.to.nn(tree, top.coords)
+tc.colors <- c('#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99')
+p <- ggtree(tree, layout='fan') +
+  geom_balance(node=tc.nn[1], fill=tc.colors[1], alpha=0.6) +
+  geom_balance(node=tc.nn[2], fill=tc.colors[2], alpha=0.6) +
+  geom_balance(node=tc.nn[3], fill=tc.colors[3], alpha=0.6) +
+  geom_balance(node=tc.nn[4], fill=tc.colors[4], alpha=0.6) +
+  geom_balance(node=tc.nn[5], fill=tc.colors[5], alpha=0.6)
+p <- annotate_balance(tree, 'n16', p=p, labels = c('n16+', 'n16-'),
+                      offset.text=0.15, bar=FALSE)
+annotate_balance(tree, 'n730', p=p, labels = c('n730+', 'n730-'),
+                 offset.text=0.15, bar=FALSE)
+
+## --------------------------------------------------------------------------
+ps.philr.long <- convert_to_long(ps.philr, get_variable(ps, 'human')) %>%
+  filter(coord %in% top.coords)
+
+ggplot(ps.philr.long, aes(x=labels, y=value)) +
+  geom_boxplot(fill='lightgrey') +
+  facet_grid(.~coord, scales='free_x') +
+  xlab('Human') + ylab('Balance Value') +
+  theme_bw()
+
+## ---- message=FALSE, warning=FALSE-----------------------------------------
+library(tidyr); packageVersion('tidyr')
+
+ps.philr.long %>%
+  rename(Human=labels) %>%
+  filter(coord %in% c('n16', 'n730')) %>%
+  spread(coord, value) %>%
+  ggplot(aes(x=n16, y=n730, color=Human)) +
+  geom_point(size=4) +
+  xlab(tc.names['n16']) + ylab(tc.names['n730']) +
+  theme_bw()
