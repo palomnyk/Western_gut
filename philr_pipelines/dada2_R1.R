51488eff239fdfdf569351d4876baa309bdb3017
Aaron_Y <amyerke@uncc.edu>
2020-02-28 14:12:31 -0500

    code for just running DADA2 on read 1

---
+++ philr_pipelines/dada2_R1.R
@@ -0,0 +1,48 @@
+# Author: Aaron Yerke
+# This is a pipeline that was created to process Western gut data through DADA2.
+# Many resources were used to create this pipeline:
+#   DADA2:
+#     https://benjjneb.github.io/dada2/tutorial.html
+
+rm(list = ls()) #clear workspace
+library("dada2")
+
+home_dir = file.path('~','git','Western_gut')
+f_path <- "~/git/Western_gut/sequences" # CHANGE ME to the directory containing the fastq files after unzipping.
+output_dir = file.path('~','git','Western_gut', 'output',"dada2")
+
+# Forward and reverse fastq filenames have format: SAMPLENAME_1.fastq and SAMPLENAME_2.fastq
+fnFs <- sort(list.files(f_path, pattern="_1.fastq", full.names = TRUE))
+sampleNames <- gsub("_1.fastq", "", fnFs, fixed=TRUE)
+fnFs <- file.path(f_path, fnFs)
+filt_path <- file.path(f_path, "filtered") # Place filtered files in filtered/ subdirectory
+filtFs <- file.path(filt_path, paste0(sampleNames, "_R1_filt.fastq"))
+
+#Filter
+out <- filterAndTrim(fnFs, filtFs,truncLen=240,
+                     maxN=0, maxEE=c(2), truncQ=2, rm.phix=TRUE,
+                     compress=FALSE, multithread=TRUE) 
+
+dds <- vector("list", length(sampleNames))
+names(dds) <- sampleNames
+
+index <-1 
+
+for (f in filtFs){
+  errF <- learnErrors(f, multithread = FALSE)
+  derepFs <- derepFastq(f, verbose=TRUE)
+  dadaFs <- dada(derepFs, err=errF, multithread=FALSE)
+  dds[[index]]<-dadaFs
+  index<-index+1
+}
+
+seqtab <- makeSequenceTable(dds)
+
+#Removing chimeras
+seqtab <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE)
+
+saveRDS(seqtab, paste0(output,"/ForwardsReads_DADA2.rds"))
+write.table(seqtab,file=paste0(output,"/ForwardReads_DADA2.txt"),sep="\t")
+
+
+
