07f8e66d07ee2e53dfc8e0d959869b6a92894a69
Aaron_Y <amyerke@uncc.edu>
2020-03-04 21:23:13 -0500

    add a save to it

--- philr_pipelines/dada2_phangorn_phyloseq_philr.R
+++ philr_pipelines/dada2_phangorn_phyloseq_philr.R
@@ -73,6 +73,10 @@ seqtab <- makeSequenceTable(dds)
 #Removing chimeras
 seqtab <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE)
 
+saveRDS(seqtab, paste0(output_dir,"dada2/ForwardsReads_DADA2.rds"))
+write.table(seqtab,file=paste0(output_dir,"dada2/ForwardReads_DADA2.txt"),sep="\t")
+
+
 fastaRef <- file.path(home_dir, 'philr_pipelines', "taxonomy", "./rdp_train_set_16.fa.gz")
 taxTab <- assignTaxonomy(seqtabNoC, refFasta = fastaRef, multithread=TRUE)
 unname(head(taxTab))
