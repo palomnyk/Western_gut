08a56d8227efb3c9cf7dddadc6a25ec1316b31a1
Aaron_Y <amyerke@uncc.edu>
2020-03-25 12:37:17 -0400

    broke dada2_phangorn_phyloseq_philr.R into 3 scripts for managing libraries

---
+++ philr_pipelines/philr_2.R
@@ -0,0 +1,81 @@
+# Author: Aaron Yerke
+# This is a pipeline that was created to explore the philr transform.
+# Many resources were used to create this pipeline:
+#   Processing the sequences through DADA2, making trees with phangorn, and putting them into phyloseq objects:
+#     This is by far the biggest source:
+#     https://github.com/spholmes/F1000_workflow/blob/master/MicrobiomeWorkflow/MicrobiomeWorkflowII.Rmd
+#     or (in article format)
+#     https://f1000research.com/articles/5-1492/v2
+#     
+#   DADA2:
+#     https://benjjneb.github.io/dada2/tutorial.html
+#   
+#   Using philr:
+#     https://bioconductor.org/packages/release/bioc/html/philr.html
+#     Code example:
+#       https://bioconductor.org/packages/release/bioc/vignettes/philr/inst/doc/philr-intro.R
+
+rm(list = ls()) #clear workspace
+
+# ‘ape’, ‘dplyr’, ‘reshape2’, ‘plyr’
+# .cran_packages <- c("ggplot2", "gridExtra")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# if (!requireNamespace("phangorn", quietly = TRUE))
+#   install.packages("phangorn")  
+# BiocManager::install("phyloseq")
+# BiocManager::install("DECIPHER")
+
+library("DECIPHER")
+library("phangorn")
+library("phyloseq")
+
+home_dir = file.path('~','git','Western_gut')
+#home_dir = file.path('cloud','project')
+output_dir = file.path(home_dir, 'output')
+project = "RDP Western Gut"
+f_path <- file.path(home_dir, "sequences") # CHANGE ME to the directory containing the fastq files after unzipping.
+# list.files(f_path)
+
+setwd(file.path(home_dir, "philr_pipelines"))
+
+con <- gzfile("ForwardReads_DADA2.rds")
+seqtab = readRDS(con)
+
+fastaRef <- file.path(home_dir, 'philr_pipelines', "taxonomy", "./rdp_train_set_16.fa.gz")
+taxTab <- assignTaxonomy(seqtab, refFasta = fastaRef, multithread=TRUE)
+unname(head(taxTab))
+
+seqs <- getSequences(seqtab)
+
+names(seqs) <- seqs # This propagates to the tip labels of the tree
+
+alignment <- AlignSeqs(DNAStringSet(seqs), anchor=NA,verbose=FALSE)
+print("Alignment completed")
+
+phangAlign <- phyDat(as(alignment, "matrix"), type="DNA")
+dm <- dist.ml(phangAlign)
+treeNJ <- NJ(dm) # Note, tip order != sequence order
+fit = pml(treeNJ, data=phangAlign)
+fitGTR <- update(fit, k=4, inv=0.2)
+fitGTR <- optim.pml(fitGTR, model="GTR", optInv=TRUE, optGamma=TRUE,
+                    rearrangement = "stochastic", control = pml.control(trace = 0))
+print("phangorn completed")
+
+
+myMeta = read.table(file.path(home_dir,'PRJEB28687.txt'), 
+                    sep="\t", 
+                    header=TRUE, 
+                    row.names = "run_accession", 
+                    check.names = FALSE,
+                    stringsAsFactors=FALSE)
+
+ps <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE),
+               sample_data(myMeta),
+               tax_table(taxTab),
+               phy_tree(fitGTR$tree))
+# ps
+print("Created ps")
+
+setwd(file.path(home_dir, "philr_pipelines"))
+saveRDS(ps, "phyloseq_obj.rds")
