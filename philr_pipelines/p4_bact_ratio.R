8239f7a2ed00df11af614c7a5091ea08c38716b3
Aaron_Y <amyerke@uncc.edu>
2020-05-07 15:06:20 -0400

    moved or deleted files

--- philr_pipelines/p4_bact_ratio.R
+++ philr_pipelines/p4_bact_ratio.R
@@ -10,7 +10,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
   BiocManager::install("philr")
   BiocManager::install("ape")
 }
-
+library("phyloseq")
 library(philr); packageVersion("philr")
 
 ##----------------Establish directory layout------------------------##
@@ -36,12 +36,36 @@ sam_name = c()
 ratio_nodes = c()
 
 #iterate through each node and compare it to each other node
-for (i1 in 1:nrow(ps.philr)){
-  for (i2 in 1:nrow(ps.philr)){
-    
-  }
-  
-}
+# for (i1 in 1:nrow(ps.philr)){
+#   for (i2 in 1:nrow(ps.philr)){
+#     print(paste(i1,i2))
+#   }
+#   
+# }
+plot_tree(ps, "sampledodge", nodeplotdefault, ladderize="left")
 
+plot_tree(ps, "sampledodge", 
+          ladderize="left",
+          label.tips = "Genus",
+          sizebase = 200,
+          base.spacing = 0.00009,
+          text.size = 2.75)
 
+library('ape')
 
+tl = data.frame(ps@phy_tree$tip.label)
+e = data.frame(ps@phy_tree$edge
+               )
+
+plot_bar(ps, "Family", "Abundance", "Family", 
+         facet_grid="Sample.Group~."
+         )
+
+n2<-extract.clade(ps@phy_tree,73)
+
+plot_tree(n2, "sampledodge", 
+          ladderize="left",
+          # label.tips = "Genus", 
+          sizebase = 200,
+          base.spacing = 0.00009,
+          text.size = 2.75)
