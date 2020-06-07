2503d77fdee59aa86628602b34c394ba11185375
Aaron_Y <amyerke@uncc.edu>
2020-06-07 13:49:57 -0400

    shorten fasta headers to contain only genBank ids

---
+++ tree_prep/shorten_fasta_headers.R
@@ -0,0 +1,41 @@
+
+#!/usr/bin/env Rscript
+
+# makeblastdb was complaining about the length of the fasta headers
+# writing this was quicker than fixing and rerunning download_seqs.R
+
+# arg 1 is the file that we will need to fix
+
+args = commandArgs(trailingOnly=TRUE)
+
+home_dir = file.path('~','git','Western_gut')
+setwd(file.path(home_dir, "tree_prep"))
+
+
+if (exists(args[1])){
+  oldFasta = args[1]
+}else{
+  oldFasta = "treeFasta.fasta"
+}
+
+fixedFasta = paste0("fixed_", oldFasta)
+
+processFile = function(filepath) {
+  con = file(filepath, "r")
+  while ( TRUE ) {
+    line = readLines(con, n = 1)
+    if ( length(line) == 0 ) {
+      break
+    }
+    # print(line)
+    if (grepl("_", line, fixed = TRUE)){
+      id = strsplit(line, "_")[[1]][1]
+      cat(paste0(id, "\n"), file = fixedFasta, append=TRUE)
+    } else{
+      cat(paste0(line, "\n"), file = fixedFasta, append=TRUE)
+    }
+  }
+  close(con)
+}
+
+processFile(oldFasta)
