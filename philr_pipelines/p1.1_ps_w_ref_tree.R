a475c270b6d2e9db7e36458ee3bd0889544e7b7a
Aaron_Y <amyerke@uncc.edu>
2020-06-16 22:28:28 -0400

    add code for making phyloseq obj with reference tree and remove code (unused) for classifying dada2 results with silva database

---
+++ philr_pipelines/p1.1_ps_w_ref_tree.R
@@ -0,0 +1,78 @@
+# Author: Aaron Yerke
+# Convert output from p1_dada2_rd1.R to silva based taxonomy. Loosely following :
+#   http://benjjneb.github.io/dada2/training.html
+
+if (!requireNamespace("BiocManager", quietly = TRUE)){
+  install.packages("BiocManager")
+  BiocManager::install("dada2",type = "source", checkBuilt = TRUE)
+  BiocManager::install("DECIPHER")
+}
+
+library("DECIPHER")
+library("dada2")
+library("phyloseq")
+
+home_dir = file.path('~','git','Western_gut')
+#home_dir = file.path('cloud','project')
+output_dir = file.path(home_dir, 'output')
+
+setwd(home_dir)
+myMeta = read.table(file.path("fullMetadata.tsv"), 
+                    sep="\t", 
+                    header=TRUE, 
+                    row.names = "run_accession", 
+                    check.names = FALSE,
+                    stringsAsFactors=FALSE)
+
+##---------------------Import R objects-----------------------------##
+con <- gzfile(file.path( "philr_pipelines", "r_objects", "ForwardReads_DADA2.rds"))
+seqtab = readRDS(con)
+close(con)
+
+con <- gzfile(file.path( "philr_pipelines", "r_objects", "ForwardReads_DADA2_taxonomy.rds"))
+taxTab <- readRDS(con)
+close(con)
+
+print("Imported R objects")
+
+tree = read_tree(file.path(home_dir,"philr_pipelines",  "taxonomy" , "silva","viFy10M5J2nvIBpCLM-QMQ_newick.txt"))
+# plot_tree(tree, nodelabf=nodeplotblank, label.tips="taxa_names", ladderize="left")
+
+tree_key = read.table(file.path(home_dir, "tree_prep", "parsed_output.csv"),
+                      sep = ",",
+                      row.names = 1,
+                      header = T)
+unmatched_ids = c()
+
+for (i in 1:length(tree$tip.label)){
+  old_lab = tree$tip.label[i]
+  id = unlist(strsplit(old_lab, "_"))[1]
+  if (id %in% tree_key$sseqid){
+    index = which(tree_key$sseqid == id)[1]
+    new_lab = row.names(tree_key)[index]
+    tree$tip.label[i] = new_lab
+
+  }else{
+    unmatched_ids = c(unmatched_ids, id)
+  }
+  # index = which(tree_key$sseqid == id)
+  # print("not in")
+}
+print(paste("unmatched ids:", length(unmatched_ids)))
+plot_tree(tree)
+
+print(length(tree$tip.label))
+  
+tree = prune_taxa(row.names(tree_key), tree)
+
+print(length(tree$tip.label))
+
+plot_tree(tree)
+
+ps <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE),
+               sample_data(myMeta),
+               tax_table(taxTab),
+               phy_tree(tree)
+)
+
+saveRDS(ps, file.path(home_dir, "philr_pipelines", "r_objects","ref_tree_phyloseq_obj.rds"))
