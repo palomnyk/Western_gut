df2b24e46b7c51126f9c829d78b2a206436da166
Aaron_Y <amyerke@uncc.edu>
2020-09-18 20:39:35 -0400

    Some clean-up

--- philr_pipelines/p8.1_otu_ratio_table._equalize_philr.R
+++ philr_pipelines/p8.1_otu_ratio_table._equalize_philr.R
@@ -5,42 +5,51 @@
 rm(list = ls()) #clear workspace
 
 ##-------------------Load Depencencies------------------------------##
-##----------------Establish directory layout------------------------##
+##---------------------Establish constants--------------------------##
 home_dir = file.path('~','git','Western_gut')
 #home_dir = file.path('cloud','project')
 output_dir = file.path(home_dir, 'output')
 setwd(file.path(home_dir))
 
 PERCENT_ABUND = 0.7
-
 ##---------------------------Functions------------------------------##
-#returns a bool vector same length as # cols
+
+# finds columns that are above X% populated by nonzeros
+# returns a bool vector same length as # cols
 percent_abund_filt <- function(abund_percent, df) {
   or_abund = apply(df, 2, function(c){
     sum(c!=0) >= abund_percent*nrow(df)})
 }
-top_x_abund_filt <- function(top_x, df) {
-  
-}
 
-##------------Import R objects and data preprocessing---------------##
+#returns a df with highest abundance top_x number of columns
+top_x_abund_filt <- function(top_x, df) {
+  df_sums = apply(df, 2, sum)
+  df_rank = rank(df_sums, ties.method = "random")
+  return(df[,df_rank <= top_x])
+}
+##------------------Import R objects and tables---------------------##
 otu_ratios = read.table(file = file.path(home_dir, "philr_pipelines", "tables", "otu_ratio_table.csv"),
                         sep = ",")
-ps = read.table(file = file.path(home_dir, "philr_pipelines", "tables", "ref_tree_ps_philr_transform.csv"),
-                        sep = ",")
+# philr_trans = read.table(file = file.path(home_dir, "philr_pipelines", "tables", "ref_tree_ps_philr_transform.csv"),
+#                         sep = ",")
+philr_trans = read.table(file = file.path(home_dir, "philr_pipelines", "tables", "denovo_ps_philr_transform.csv"),
+                sep = ",")
+
 ##--------------------Create Ratio table----------------------------##
-print(dim(otu_ratios))
-print(dim(ps))
-ps_abund = ps[,percent_abund_filt(PERCENT_ABUND, ps)]
-or_abund = otu_ratios[,percent_abund_filt(PERCENT_ABUND,otu_ratios)]
 
-or_sums = apply(or_abund, 2, sum)
-or_rank = rank(or_sums, ties.method = "random")
-
-or_top_x = or_abund[,or_rank <= 35]
+ps_abund <- philr_trans[,percent_abund_filt(PERCENT_ABUND, philr_trans)]
+or_abund <- otu_ratios[,percent_abund_filt(PERCENT_ABUND,otu_ratios)]
+lowest_ncol <- min( c( ncol(ps_abund), ncol(or_abund)))
+or_top_x <- top_x_abund_filt(lowest_ncol, or_abund)
+ps_top_x <- top_x_abund_filt(lowest_ncol, ps_abund)
 
 write.table(or_top_x, 
             file = file.path(home_dir, "philr_pipelines", "tables", "otu_ratio_table_top_35.csv"),
             sep = ",")
 
-
+# write.table(ps_top_x,
+#             file = file.path(home_dir, "philr_pipelines", "tables", "ref_tree_ps_top_35.csv"),
+#             sep = ",")
+write.table(ps_top_x, 
+            file = file.path(home_dir, "philr_pipelines", "tables", "denovo_tree_ps_top_35.csv"),
+            sep = ",")
